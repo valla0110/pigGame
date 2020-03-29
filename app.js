3c9ff9b25e06d671cd47db321ccb0999404828dc
valla0110 <allavs01@gmail.com>
2020-03-29 20:19:12 +0530

    updating game over condition

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-let isActive , current;
+let isActive , current,gameOver;
 
 init = () =>{
     isActive = [true,false];
     current = [0,0];
-
+gameOver=false;
     document.getElementById('score-0').innerText = 0;
     document.getElementById('score-1').innerText = 0;
     document.getElementById('current-0').innerText = 0;
@@ -30,15 +30,18 @@ setActivePlayer = () =>{
 };
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
-    const player = isPlayerActive();
-    const dice = Math.floor(Math.random() * 6) + 1;
-    document.querySelector('.dice').src = 'dice-'+dice+'.png';
-    dice!==1 ? current[player]=current[player]+dice : (current[player]=0,setActivePlayer());
-    document.getElementById('current-' + player).innerText = current[player];
+    if(!gameOver){
+        const player = isPlayerActive();
+        const dice = 50//Math.floor(Math.random() * 6) + 1;
+        document.querySelector('.dice').src = 'dice-'+dice+'.png';
+        dice!==1 ? current[player]=current[player]+dice : (current[player]=0,setActivePlayer());
+        document.getElementById('current-' + player).innerText = current[player];
 
-    current[player]===100 ?
-        (document.querySelector('.dice').display ='none' ,
-            document.getElementById('name-'+player).innerText='Winner') : null;
+        current[player]===100 ?
+            (document.getElementById('dice').style.display ='none' ,
+                document.getElementById('name-'+player).innerText='Winner',gameOver=true,
+                document.getElementById('roll').style.display='none') : null;
+    }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', () => {
